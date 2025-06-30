a6e68b0e526b0aec8530d9ba62f396dbd5520af7
J Vinay <vny.grd108@gmail.com>
2025-06-30 11:41:40 +0530

    add to input account no for the sbi bank

--- sbi_to_same_bank_transfer.py
+++ sbi_to_same_bank_transfer.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, send_file
 import pandas as pd
 from datetime import datetime
 import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
@@ -9,74 +10,90 @@ PROCESSED_FOLDER = "processed"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(PROCESSED_FOLDER, exist_ok=True)
 
-@app.route('/', methods=['GET', 'POST'])
+# Allowed account numbers and their details
+ACCOUNT_MAP = {
+    "43557115725": {"name": "SBI Saksham", "ifsc": "SBIN00007773"},
+    "41256726637": {"name": "SBI Gram Seva", "ifsc": "SBIN00007773"},
+    "34889306900": {"name": "SCHCT Gorhe", "ifsc": "SBIN00007773"},
+    "40237416058": {"name": "SBI Wadi", "ifsc": "SBIN00007773"}
+}
+
+@app.route('/', methods=['GET'])
 def upload_form():
-    if request.method == 'POST':
-        return process_file()
-    return render_template('sbt.html')
+    return render_template('sbt.html', account_map=ACCOUNT_MAP)
 
 @app.route('/process', methods=['POST'])
 def process_file():
     if 'file' not in request.files:
         return "No file uploaded"
-    
+
     file = request.files['file']
     if file.filename == '':
         return "No selected file"
-    
-    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    file_path = os.path.join(UPLOAD_FOLDER, secure_filename(file.filename))
     file.save(file_path)
-    
-    # Processing the Excel file
-    same_bank_transfer = pd.read_excel(file_path, engine="xlrd", dtype={'Bank-A/C': str})
-    same_bank_transfer = same_bank_transfer[same_bank_transfer['IFSC'].str.contains('SBIN', na=False)]
-    same_bank_transfer = same_bank_transfer[['Bank-A/C', 'IFSC', 'Amount', 'Vendor']]
-    same_bank_transfer = same_bank_transfer.rename(columns={
-        'Bank-A/C': 'Account Number',
-        'IFSC': 'IFSC Code',
-        'Vendor': 'Name'
-    })
-    
+
+    account_input = request.form.get("account_input", "").strip()
+    start_serial = int(request.form.get("start_serial", 1))
+
+    if account_input not in ACCOUNT_MAP:
+        return render_template("sbt.html", error="Invalid account number", account_map=ACCOUNT_MAP)
+
+    details = ACCOUNT_MAP[account_input]
+    account_name = details["name"]
+    ifsc_code = details["ifsc"][-5:]
+
+    # Read Excel
+    ext = os.path.splitext(file_path)[1]
+    if ext == ".xlsx":
+        df = pd.read_excel(file_path, engine="openpyxl", dtype={'Bank-A/C': str})
+    elif ext == ".xls":
+        df = pd.read_excel(file_path, engine="xlrd", dtype={'Bank-A/C': str})
+    else:
+        return "Unsupported file format"
+
+    same_bank_transfer = df[df['IFSC'].str.contains('SBIN', na=False)]
+    same_bank_transfer = same_bank_transfer[['Bank-A/C', 'IFSC', 'Amount', 'Vendor']].copy()
+    same_bank_transfer.columns = ['Account Number', 'IFSC Code', 'Amount', 'Name']
     today_date = datetime.today().strftime('%d/%m/%y')
     same_bank_transfer.insert(2, 'Date', today_date)
-    
-    start_serial = int(request.form.get("start_serial", 1))
+
     total_amount = same_bank_transfer["Amount"].sum()
-    
+
     default_row = pd.DataFrame([{
-        "Account Number": "41256726637",
-        "IFSC Code": "07773",
+        "Account Number": account_input,
+        "IFSC Code": ifsc_code,
         "Date": today_date,
         "Amount": total_amount,
-        "Name": "SCST GORHE",
+        "Name": account_name,
         "Serial": start_serial,
         "Mode": "",
         "Formula": ""
     }])
-    
+
     same_bank_transfer = pd.concat([default_row, same_bank_transfer], ignore_index=True)
     same_bank_transfer["Serial"] = range(start_serial, start_serial + len(same_bank_transfer))
     same_bank_transfer["IFSC Code"] = same_bank_transfer["IFSC Code"].astype(str).str[-5:]
-    
-    same_bank_transfer.loc[0, "Formula"] = same_bank_transfer.loc[0, "Account Number"] + "#" + \
-                                            same_bank_transfer.loc[0, "IFSC Code"] + "#" + \
-                                            same_bank_transfer.loc[0, "Date"] + "#" + \
-                                            str(same_bank_transfer.loc[0, "Amount"]) + "##" + \
-                                            str(same_bank_transfer.loc[0, "Serial"]) + "#" + \
-                                            same_bank_transfer.loc[0, "Name"] + "#"
-    
+
+    same_bank_transfer.loc[0, "Formula"] = f"{same_bank_transfer.loc[0, 'Account Number']}#" \
+                                           f"{same_bank_transfer.loc[0, 'IFSC Code']}#" \
+                                           f"{same_bank_transfer.loc[0, 'Date']}#" \
+                                           f"{same_bank_transfer.loc[0, 'Amount']}##" \
+                                           f"{same_bank_transfer.loc[0, 'Serial']}#" \
+                                           f"{same_bank_transfer.loc[0, 'Name']}#"
+
     same_bank_transfer.loc[1:, "Formula"] = same_bank_transfer.loc[1:, "Account Number"] + "#" + \
                                             same_bank_transfer.loc[1:, "IFSC Code"] + "#" + \
                                             same_bank_transfer.loc[1:, "Date"] + "##" + \
                                             same_bank_transfer.loc[1:, "Amount"].astype(str) + "#" + \
                                             same_bank_transfer.loc[1:, "Serial"].astype(str) + "#" + \
                                             same_bank_transfer.loc[1:, "Name"] + "#"
-    
-    date_str = datetime.today().strftime('%d.%m.%Y')
-    output_filename = f"SCHCT_{date_str}_Same_Bank_Transfer.xlsx"
+
+    output_filename = f"SCHCT_{datetime.today().strftime('%d.%m.%Y')}_Same_Bank_Transfer.xlsx"
     output_path = os.path.join(PROCESSED_FOLDER, output_filename)
     same_bank_transfer.to_excel(output_path, index=False)
-    
+
     return send_file(output_path, as_attachment=True)
 
 if __name__ == '__main__':
